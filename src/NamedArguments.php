f4a8436a66c00ddf4d455df42b9c913d4d688692
Luís Henriques <luis.henriques@uniplaces.com>
2020-09-20 10:13:04 +0100

    Make named arguments work

--- src/NamedArguments.php
+++ src/NamedArguments.php
@@ -6,6 +6,6 @@ class NamedArguments
 {
     public static function getUnionTypesNamedArguments(): ?UnionTypes
     {
-        return new UnionTypes(arg2: 2);
+        return new UnionTypes(arg3: 2, arg1: 'abc');
     }
 }
--- src/UnionTypes.php
+++ src/UnionTypes.php
@@ -7,16 +7,18 @@ namespace App;
  */
 class UnionTypes
 {
-    private ?string $arg1;
+    private string $arg1;
     private int|float|null $arg2;
+    private ?int $arg3;
 
-    public function __construct(?string $arg1, int|float $arg2 = null)
+    public function __construct(string $arg1, int|float $arg2 = null, int $arg3 = null)
     {
         $this->arg1 = $arg1;
         $this->arg2 = $arg2;
+        $this->arg3 = $arg3;
     }
 
-    public function getArg1(): ?string
+    public function getArg1(): string
     {
         return $this->arg1;
     }
@@ -26,8 +28,13 @@ class UnionTypes
         return $this->arg2;
     }
 
+    public function getArg3(): int|null
+    {
+        return $this->arg3;
+    }
+
     public function getArgs(): string
     {
-        return "arg1: $this->arg1, arg2: $this->arg2";
+        return "arg1: $this->arg1, arg2: $this->arg2, arg3: $this->arg3";
     }
 }
